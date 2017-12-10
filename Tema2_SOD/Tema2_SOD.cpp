25187f0fe02d27eb4462f927bb4d75eb67d5372b
ADMINRG-K27CGIO\alecs <alecu89@yahoo.com>
2017-12-10 18:45:21 +0200

    added OpenCV

--- Tema2_SOD/Tema2_SOD.cpp
+++ Tema2_SOD/Tema2_SOD.cpp
@@ -1,11 +1,34 @@
-// Tema2_SOD.cpp : Defines the entry point for the console application.
-//
+#include <omp.h>  
+#include <stdio.h>  
+#include <iostream>
 
-#include "stdafx.h"
-
+using namespace std;
 
 int main()
 {
-    return 0;
-}
+	int a[5], i;
+	char stop[100];
 
+#pragma omp parallel  
+	{
+		// Perform some computation.  
+#pragma omp for  
+		for (i = 0; i < 5; i++)
+			a[i] = i * i;
+
+		// Print intermediate results.  
+#pragma omp master  
+		for (i = 0; i < 5; i++)
+			printf_s("a[%d] = %d\n", i, a[i]);
+
+		// Wait.  
+#pragma omp barrier  
+
+		// Continue with the computation.  
+#pragma omp for  
+		for (i = 0; i < 5; i++)
+			a[i] += i;
+
+		cin.get();
+	}
+}
