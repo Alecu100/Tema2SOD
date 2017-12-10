198b0b8c64f5b0ba080a47d2b0ff8456d19fad68
ADMINRG-K27CGIO\alecs <alecu89@yahoo.com>
2017-12-10 19:02:25 +0200

    added openCV references to project

--- Tema2_SOD/Tema2_SOD.cpp
+++ Tema2_SOD/Tema2_SOD.cpp
@@ -1,11 +1,29 @@
 #include <omp.h>  
 #include <stdio.h>  
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	string input;
+
+	while (true) {
+		cout << "Do you want to execute another action?" << endl;
+		cout << "1. Yes" << endl;
+		cout << "2. No" << endl;
+
+		cin >> input;
+
+		if (input == "2" || input == "No") {
+			break;
+		}
+
+		cout << "Please enter the path to the image";
+		cin >> input;
+	}
+
 	int a[5], i;
 	char stop[100];
 
@@ -28,7 +46,5 @@ int main()
 #pragma omp for  
 		for (i = 0; i < 5; i++)
 			a[i] += i;
-
-		cin.get();
 	}
 }
